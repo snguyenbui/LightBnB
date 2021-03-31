cf0e8578d2ccb240a21cc4d64dce45627c32e9fa
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-31 19:47:06 +0000

    Create properties table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -4,3 +4,22 @@ CREATE TABLE users (
   email VARCHAR(255),
   password VARCHAR(255)
 );
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN
+);

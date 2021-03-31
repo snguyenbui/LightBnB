8a784b859393bf2fdee1da31b59c4a6e63913d3b
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-31 19:51:42 +0000

    Created property_reviews table

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -31,3 +31,12 @@ CREATE TABLE reservations (
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT,
+  message TEXT
+);

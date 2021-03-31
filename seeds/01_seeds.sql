dd197f9d06bb830b47c73d392932f768aa6ddfc2
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-31 20:13:21 +0000

    Added users data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password)
+VALUES ('Illistil Calen', 'illistilcalen@cot.fc.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Jisooya Fhod', 'jisooyafhod@cot.fc.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Samirra Zhys', 'samirrazhys@cot.fc.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Erika Okibi', 'erkiaokibi@cot.fc.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Jarad Dragonstone', 'jaraddragonstone@cot.fc.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Roxax Saxas', 'roxaxsaxas@cot.fc.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Reina Eeva', 'reinaeeva@cot.fc.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');

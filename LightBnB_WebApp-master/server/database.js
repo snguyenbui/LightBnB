66ab264052bdb4c89b324d4483ae0afa4d2af9f0
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-01 21:07:38 +0000

    modified addProperty to use postgres

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -170,9 +170,19 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+  const propertyParams = []
+  for (const param in property) {
+    propertyParams.push(property[param]);
+  }
+  return pool.query(`
+  INSERT INTO properties (title, description, number_of_bedrooms,number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `, propertyParams)
+  .then(res => res.rows);
 }
 exports.addProperty = addProperty;

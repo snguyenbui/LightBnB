f7ca403b6c86578d5d271babae24a2cf8fa5ddff
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-01 19:34:06 +0000

    modified getAllReservations to use node postgres

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -66,7 +66,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*, avg(rating) AS average_rating
+  FROM properties
+  JOIN reservations ON properties.id = property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE (now()::date > end_date AND reservations.guest_id = $1)
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => res.rows)
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
